3782edbfb33c5528f9c409513f3bc1c439a05386
Muhammed Küçükaslan <kucukaslanmuhammed@gmail.com>
2024-06-03 06:24:14 +0300

    deneme

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const server = express();
 
 server.use(express.json());
 
+import todoRouter from "./router/todo-router.js";
+
+server.use("/todos", todoRouter);
+
+
 server.listen(3000, () => { "Server is running on port 3000" })
