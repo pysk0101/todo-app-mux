f6d8c33429cd1fee90f4649c9115915023434ec0
Muhammed Küçükaslan <kucukaslanmuhammed@gmail.com>
2024-06-03 06:35:34 +0300

    Put, delete, post missions added

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,5 +1,37 @@
 import {todoSchema,todos} from "../models/todo-model.js";
 
+
+const changeTodo = (req,res) => {
+    const todo = todos.find(todo => todo.id === parseInt(req.params.id))
+    if (!todo) return res.status(404).send("The todo with the given ID was not found.")
+    const {error} = todoSchema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+    let oldTodo = Object.assign({}, todo)
+    todo.mission = req.body.mission
+    res.send(oldTodo, todo)
+}
+
+const deleteTodo= (req,res) => {
+    const todo = todos.find(todo => todo.id === parseInt(req.params.id))
+    if (!todo) return res.status(404).send("The todo with the given ID was not found.")
+    const index = todos.indexOf(todo)
+    todos.splice(index, 1)
+    res.send(`The todo with the ID ${req.params.id} was deleted.`)
+}
+
+const createTodo = (req,res) => {
+    const {error} = todoSchema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+    const todo = {
+        id: todos.length + 1,
+        mission: req.body.mission,
+        isCompleted: req.body.isCompleted
+    }
+    todos.push(todo)
+    res.send(todo)
+
+}
+
 const getTodosById = (req,res) =>{
     const todo = todo.find(todo => todo.id === parseInt(req.params.id))
     if (!todo) return res.status(404).send("The todo with the given ID was not found.")
@@ -11,4 +43,4 @@ const getTodos = (req, res) => {
 }
 
 
-export {getTodosById, getTodos}
+export {getTodosById, getTodos, createTodo, deleteTodo, changeTodo}
--- router/todo-router.js
+++ router/todo-router.js
@@ -1,6 +1,11 @@
 import express from "express";
 const todoRouter = express.Router();
-import {getTodosById, getTodos} from "../controllers/todo-controller.js";
+import {getTodosById, getTodos, createTodo, deleteTodo, changeTodo} from "../controllers/todo-controller.js";
+
+
+todoRouter.put("/:id", changeTodo);
+todoRouter.delete("/:id", deleteTodo);
+todoRouter.post("/", createTodo);
 
 todoRouter.get("/:id", getTodosById);
 todoRouter.get("/", getTodos);
