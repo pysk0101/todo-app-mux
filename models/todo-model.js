d8b617f32eec660f67c1aadb3643ad69c7503f1a
Muhammed Küçükaslan <kucukaslanmuhammed@gmail.com>
2024-06-03 15:09:35 +0300

    some fixed

--- models/todo-model.js
+++ models/todo-model.js
@@ -4,7 +4,7 @@ import fs from "fs"
 const todoSchema = Joi.object({
     'id': Joi.number(),
     'mission': Joi.string().required(),
-    "isCompleted": Joi.boolean().required(),
+    "isCompleted": Joi.boolean(),
 })
 
 const todos = JSON.parse(fs.readFileSync("./db/todo-database.json"))
