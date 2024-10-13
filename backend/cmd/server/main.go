0b494356b5239c6de9fba411b7f7cad595123b31
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-13 15:08:33 +0300

    main updated

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/pysk0101/todo-app-mux/backend/api"
+	"github.com/pysk0101/todo-app-mux/backend/internal/adapters/db"
+	"github.com/pysk0101/todo-app-mux/backend/internal/adapters/repositories"
+	"github.com/pysk0101/todo-app-mux/backend/internal/adapters/services"
+)
+
+func main() {
+	// .env dosyasını yükle
+	if err := godotenv.Load("../../.env"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading .env file: %s\n", err)
+		os.Exit(1)
+	}
+
+	db.InitDB()
+
+	todoRepository := repositories.NewTodoRepositoryImpl(db.GetDB())
+	todoService := services.NewTodoServiceImpl(todoRepository)
+
+	router := mux.NewRouter()
+
+	api.NewRouter(todoService)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server is running on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
