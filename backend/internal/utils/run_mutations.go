ab75e1baf3421065db6074bd754615f9190c7cb7
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-13 18:17:23 +0300

    added: run mutations util

--- backend/internal/utils/run_mutations.go
+++ backend/internal/utils/run_mutations.go
@@ -1,1 +1,24 @@
 package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pysk0101/todo-app-mux/backend/internal/adapters/db"
+)
+
+func RunMigrations() {
+	// Migration dosyasını oku
+	migrationFile, err := os.ReadFile("../../../backend/internal/adapters/db/migrations/create_tables.sql")
+	if err != nil {
+		log.Fatalf("Error reading migration file: %v", err)
+	}
+
+	// Veritabanında migration'ı çalıştır
+	if _, err := db.Database.Exec(string(migrationFile)); err != nil {
+		log.Fatalf("Error executing migration: %v", err)
+	}
+
+	fmt.Println("Migrations completed successfully")
+}
