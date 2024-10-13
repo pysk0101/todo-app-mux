18134094aa52d176e9bc24081610eb28bacf8270
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-13 15:04:31 +0300

    todo domain created

--- backend/internal/core/domain/todo.go
+++ backend/internal/core/domain/todo.go
@@ -1,1 +1,8 @@
 package domain
+
+type Todo struct {
+	Id         string `json:"id"`
+	Title      string `json:"title"`
+	IsDone     bool   `json:"is_done"`
+	Created_At string `json:"created_at"`
+}
--- backend/internal/core/ports/todo_service.go
+++ backend/internal/core/ports/todo_service.go
@@ -1,1 +1,10 @@
 package ports
+
+import "github.com/pysk0101/todo-app-mux/backend/internal/core/domain"
+
+type TodoService interface {
+	GetAll() ([]*domain.Todo, error)
+	Create(todo *domain.Todo) error
+	Update(todo *domain.Todo) error
+	Delete(id string) error
+}
